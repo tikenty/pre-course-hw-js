943e841ac7fb27251ca4df9890392cb13aa0c583
Tlek <tikenty@gmail.com>
2024-02-14 00:46:23 +0600

    finished 2 task

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,7 @@
-// code
+let number1 = Math.floor(Math.random() * 100)
+result = number1 % 2
+if (result === 0) {
+    console.log(`«[${number1}]:Это четное число»`)
+} else {
+    console.log(`«[${number1}]:Это нечетное число»`)
+}// code
