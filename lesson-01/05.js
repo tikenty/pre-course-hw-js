4d24c1f6219983010108032a8687af9ec1c87b4d
Tlek <tikenty@gmail.com>
2024-02-19 22:24:26 +0600

    fixed solution number 7

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -13,18 +13,20 @@
 
 let rule = "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
 
-function ruleLength(rule) {
-    switch (true) {
-        case rule.length < 25: {
-            return "Все таки нет правил без исключения";
-        }
-        case rule.length > 25: {
-            return "У меня 100 пудов все получится";
-        }
-        default: {
-            return "50 на 50";
-        }
+
+switch (true) {
+    case rule.length < 25: {
+        console.log("Все таки нет правил без исключения");
+        break;
+    }
+    case rule.length > 25: {
+        console.log("У меня 100 пудов все получится");
+        break;
+    }
+    default: {
+        console.log("50 на 50");
     }
 }
 
-console.log(ruleLength(rule))
+
+
