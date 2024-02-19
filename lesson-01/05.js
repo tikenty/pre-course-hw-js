420b8355fcaeac6f21ece6713785872469b18aba
Tlek <tikenty@gmail.com>
2024-02-19 21:26:59 +0600

    fixed solution for 5 task

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -13,17 +13,14 @@
 
 let rule = "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
 
-let result = rule.length;
-console.log(result)
-
-switch(true) {
-    case result < 25:
+switch (true) {
+    case rule.length < 25:
         console.log('Все таки нет правил без исключения');
         break;
-    case result > 25:
+    case rule.length > 25:
         console.log('У меня 100 пудов все получится');
         break;
-    case result === 25:
+    case rule.length === 25:
         console.log('50 на 50');
         break;
     default:
