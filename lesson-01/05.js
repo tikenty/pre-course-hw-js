d596b636eb7f10713be43c1a1bb80623b859f7fa
Tlek <tikenty@gmail.com>
2024-02-19 21:44:01 +0600

    fixed new solution for 5 task

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -12,13 +12,21 @@
 //     }
 
 let rule = "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
-switch (true) {
-    case rule.length < 25:
-        console.log("Все таки нет правил без исключения");
-        break;
-    case rule.length > 25:
-        console.log("У меня 100 пудов все получится");
-        break;
-    default: console.log("«50 на 50»");
-        break;
+
+function ruleLength(rule) {
+    switch (true) {
+        case rule.length < 25:
+            console.log("Все таки нет правил без исключения");
+            break;
+        case rule.length > 25:
+            console.log("У меня 100 пудов все получится");
+            break;
+        default:
+            console.log("50 на 50");
+    }
 }
+
+ruleLength('Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.')
+ruleLength('Еще не родился')
+ruleLength('Еще не родился тот челове')
+
