d4401f7801466f8d155047980962d7a4d45c1084
Tlek <tikenty@gmail.com>
2024-02-19 22:08:15 +0600

    fixed solution number 5

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -13,14 +13,21 @@
 
 let rule = "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
 
-switch (true) {
-    case rule.length < 25:
-        console.log("Все таки нет правил без исключения");
-        break;
-    case rule.length > 25:
-        console.log("У меня 100 пудов все получится");
-        break;
-    default:
-        console.log("50 на 50");
-        break;
+function ruleLength(rule) {
+    switch (true) {
+        case rule.length < 25:
+            return "Все таки нет правил без исключения";
+        case rule.length > 25:
+            return "У меня 100 пудов все получится";
+        default:
+            return "50 на 50";
+    }
 }
+
+function launchRuleLength(rule) {
+    console.log(ruleLength(rule));
+    console.log(ruleLength("Еще не"));
+    console.log(ruleLength("Еще не родился тот челове"));``
+}
+
+launchRuleLength(rule)
