9f0e6a93d0751f71a7235be13d0d515556718bd8
Tlek <tikenty@gmail.com>
2024-02-26 23:21:10 +0600

    Revert "added solution for 2 lesson task 7"
    This reverts commit 742b56b89f77fcc7ee8e1ab70506fdc64144d972.

--- lesson-01/07.js
+++ lesson-01/07.js
@@ -1,3 +1,9 @@
+// let iAm = 'я'
+// let krasavchik = "красавчик!";
+// 📋
+//   Выведи в консоль «Я красавчик!».
+// ❗ Обрати внимание: «Я» с заглавной.
+
 let iAm = 'я'
 let krasavchik = "красавчик!";
 console.log(`${iAm.toUpperCase()} ${krasavchik}`)
