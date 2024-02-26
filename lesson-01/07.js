742b56b89f77fcc7ee8e1ab70506fdc64144d972
Tlek <tikenty@gmail.com>
2024-02-26 23:08:12 +0600

    added solution for 2 lesson task 7

--- lesson-01/07.js
+++ lesson-01/07.js
@@ -1,9 +1,3 @@
-// let iAm = 'я'
-// let krasavchik = "красавчик!";
-// 📋
-//   Выведи в консоль «Я красавчик!».
-// ❗ Обрати внимание: «Я» с заглавной.
-
 let iAm = 'я'
 let krasavchik = "красавчик!";
 console.log(`${iAm.toUpperCase()} ${krasavchik}`)
