97c244f8f8489972a49daad6d9674797ea512037
Tlek <tikenty@gmail.com>
2024-02-26 23:24:06 +0600

    fix

--- lesson-02/04.js
+++
@@ -1,9 +0,0 @@
-const giveMeRightCondition = (value1, value2, maximum) => {
-    if (value1 === value2 && value2 <=  maximum) {
-        console.log('Success!');
-    } else {
-        console.log('Something wrong!');
-    }
-}
-
-giveMeRightCondition(5, 4, 3)
