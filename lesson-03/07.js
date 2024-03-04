a538f038a40d0ff23997e266456dcde9580b8db7
Tlek <tikenty@gmail.com>
2024-03-04 23:50:50 +0500

    added solution for lesson 3 task 7 fixed

---
+++ lesson-03/07.js
@@ -0,0 +1,11 @@
+const numbers = [
+    [1, 2, 3, 4, 5],
+    [11, 20, 33, 40, 55],
+    [111, 200, 333, 400, 555],
+];
+
+const filteredNumbers = numbers.map(index => {
+    return index.filter(array => array % 2 ===0);
+})
+
+console.log(filteredNumbers)
